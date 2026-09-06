ef46a7ad4b381cf7a301899bee00fec99f5e37a7
Nakata Maho <maho.nakata@gmail.com>
2026-09-06 09:46:45 +0900

    ci: consume the current upstream snapshot archive format

--- ci_build.sh
+++ ci_build.sh
@@ -12,22 +12,22 @@ rm -rf _output
 mkdir _output
 
 # GCC
-git clean -x -d -f -e _output -e _watcom -e ow-snapshot.tar.gz
+git clean -x -d -f -e _output -e _watcom -e ow-snapshot.tar.xz
 make all COMPILER=gcc
 mv -i bin/KGC*.map bin/KGC*.sys _output/.
 
 # Watcom
 if [ ! -d _watcom ] ; then
-  [ -f ow-snapshot.tar.gz ] || wget --quiet https://github.com/open-watcom/open-watcom-v2/releases/download/Current-build/ow-snapshot.tar.gz
+  [ -f ow-snapshot.tar.xz ] || wget --quiet https://github.com/open-watcom/open-watcom-v2/releases/download/Current-build/ow-snapshot.tar.xz
 
   mkdir _watcom
-  (cd _watcom && tar -xf ../ow-snapshot.tar.gz)
+  (cd _watcom && tar -xf ../ow-snapshot.tar.xz)
 fi
 
 export PATH=$TRAVIS_BUILD_DIR/bin:$PATH:$TRAVIS_BUILD_DIR/_watcom/binl64
 export WATCOM=$TRAVIS_BUILD_DIR/_watcom
 
-git clean -x -d -f -e _output -e _watcom -e ow-snapshot.tar.gz
+git clean -x -d -f -e _output -e _watcom -e ow-snapshot.tar.xz
 make all COMPILER=owlinux
 mv -i bin/KWC*.map bin/KWC*.sys _output/.
 
