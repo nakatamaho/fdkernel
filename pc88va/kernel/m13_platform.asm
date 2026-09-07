4504c0055fcadbefa3f55521ae1f2581cbb1bdcc
Nakata Maho <maho.nakata@gmail.com>
2026-09-07 11:05:37 +0900

    pc88va: fix M13 adapter include paths

--- pc88va/kernel/m13_platform.asm
+++ pc88va/kernel/m13_platform.asm
@@ -6,8 +6,8 @@
 bits 16
 cpu 8086
 
-%include "../../kernel/segs.inc"
-%include "../../hdr/stacks.inc"
+%include "../kernel/segs.inc"
+%include "../hdr/stacks.inc"
 %include "../boot/loader_abi.inc"
 
 %ifndef PC88VA
